1bcc3004d4b2e0dc2855043a65c7bc713768a176
Adam Zakwan <azakwanzul@gmail.com>
2025-09-17 13:31:20 -0400

    chore: add GitHub Pages deploy workflow and set router basename

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/study/:deckId" element={<StudyMode />} />
           <Route path="*" element={
